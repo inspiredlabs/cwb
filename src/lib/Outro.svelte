99b9f9e2215337f8cc355166386f32ee073f9091
Scott Phillips <alan.scott.phillips@protonmail.ch>
2021-08-30 13:28:56 +0200

    Rm `<b>`& `newlines` inside html & content update for SERP.

--- src/lib/Outro.svelte
+++ src/lib/Outro.svelte
@@ -6,16 +6,14 @@
     <!-- --TODO: Courage, Care, Capacity Building, Courses -->
     <h3 class="f4">About&nbsp;Us<!-- Clowns Without&nbsp;Bordersf&hellip; --></h3>
     <p class="lh-copy measure mt4 mt0-ns">
-      <b>We work in Humanitarian Aid delivery on behalf of children in&nbsp;disaster.</b>We develop relevant, inspiring activities that support behaviour&nbsp;change.
+      <strong>We work in Humanitarian Aid delivery on behalf of children in&nbsp;disaster.</strong> We&nbsp;develop relevant, inspiring activities that support behaviour&nbsp;change.
     </p><!-- /contents -->
   </div><!-- /column1 -->
 
-
-
-
   <div class="fn fl-l w-50-l pl4-l"><!-- Col2 -->
     <h3 class="f4">Improving Programme Delivery</h3>
-    <p class="lh-copy measure mt4 mt0-ns"><b>
-      <a href="/courses" class="underline b link o-80 glow inherit hover-o-100 underline-hover transition">Our Courses</a> train teams to improve programme delivery outcomes already running</b> and&nbsp;assess children and young people's learning using our Monitoring and Evaluation&nbsp;Toolkit.</p><!-- /contents -->
+    <p class="lh-copy measure mt4 mt0-ns"><strong>
+      <a href="/courses" class="underline b link o-80 glow inherit hover-o-100 underline-hover transition">Our Courses</a> train teams to improve programme delivery</strong>&nbsp;measure impact &amp; focus on long-term progress
+      using our Monitoring and Evaluation&nbsp;Toolkit.</p><!-- /contents -->
   </div><!-- /Column2 -->
 </section>
