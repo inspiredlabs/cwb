26d89fb4ffe547c55f46e5e39c45e186a1d98d1e
Scott Phillips <alan.scott.phillips@protonmail.ch>
2022-01-28 20:02:31 +0100

    Update __news.svelte

--- src/lib/__news.svelte
+++ src/lib/__news.svelte
@@ -66,23 +66,24 @@ export let title, objective, tags, author, location, images, serp, ethno, date, 
 -->
 <div class="w-100 pv5 tc "><!-- bg-primary-back -->
 	<Section>
+    <!-- `target`: https://www.youtube.com/watch?v=0hNDDpljJrs -->
     <!-- "semantically" "social networking" "sharing widget" "html" -->
     <!-- learn: A/B test this on audience -->
     <!-- <h4 class="primary-back">Who could you <span class="accent">share&nbsp;this?</span></h4> -->
 		<header class="primary-back bc"><span class="accent">Share</span> article on:</header><!-- ttu tracked -->
 		<ul class="list pl0 tracked tracked-m tracked-l ttu mb3 b ">
 			<li class="f8 f7-ns f5-m f4-l dib"><!-- ph0 pv2 ph2-ns pv2-ns  --><a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcwb.org.uk{$page.path}%2F&amp;quote=Article%20by%20{ encodeURI(`${author}`) },%20Clowns%20Without%20Borders"
-      target="_blank" title="Share on Facebook"
+      target="_blank" rel="noreferrer" title="Share on Facebook"
       class="pointer link f8 f6-ns f6-m f6-l fl-m pv2 pv1-m ph1 ph3-ns ph3-m ph4-l mr2 ba bw1 b--secondary-back white bg-facebook-alt ttu tracked tracked-ns tracked-m tracked-mega-l dib system tc mv1 hover-o-80 o-transition">Facebook</a><!-- br1 o-transition hover-o-100 o-80 -->
         <!-- Static: stackoverflow.com/questions/5478702/add-facebook-share-button-to-static-html-page -->
       </li>
 			<li class="f8 f7-ns f5-m f4-l dib"><a href="https://twitter.com/intent/tweet?source=https%3A%2F%2Fcwb.org.uk{$page.path}%2F&amp;text=https%3A%2F%2Fcwb.org.uk{$page.path}"
-        target="_blank" title="Tweet this"
+        target="_blank" rel="noreferrer" title="Tweet this"
         class="pointer link f8 f6-ns f6-m f6-l fl-m pv2 pv1-m ph1 ph3-ns ph3-m ph4-l mr2 ba bw1 b--secondary-back white bg-twitter-alt ttu tracked tracked-ns tracked-m tracked-mega-l dib system tc mv1 hover-o-80 o-transition">twitter</a><!-- %0A%0A{encodeURI(`${serp}`)} --><!-- br1 o-transition hover-o-100 o-80 --></li>
 			<li class="f8 f7-ns f5-m f4-l dib">
         <!-- { encodeURI(`${title}`) }%20%E2%80%93%20by%20{ encodeURI(`${author}`) }%3A%0A%0A -->
         <a href="mailto:?subject=Clowns%20Without%20Borders%20article%20by%20{ encodeURI(`${author}`) }&amp;body=https%3A%2F%2Fcwb.org.uk{$page.path}"
-					target="_blank" title="email this"
+					target="_blank" rel="noreferrer" title="email this"
 					class="pointer link f8 f6-ns f6-m f6-l fl-m pv2 pv1-m ph1 ph3-ns ph3-m ph4-l mr2 ba bw1 b--secondary-back white bg-pm-blue-dark ttu tracked tracked-ns tracked-m tracked-mega-l dib system tc mv1 hover-o-80 o-transition">email</a><!-- br1 o-transition hover-o-100 o-80 -->
         </li>
 		</ul>
