2d290374a98fb5db92d58b345f7974e3de01e503
Scott Phillips <alan.scott.phillips@protonmail.ch>
2022-04-09 09:16:43 +0200

    Update SVG `defs` and `Hero.svelte` strapline idea
    `Hero.svelte` strapline idea: "Laughter. It's about the child. Clowns Without Borders  is able to respond in the early phase of a crisis with psychosocial support for children,  such as our work in Ukraine."

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -4,8 +4,12 @@
 <!-- pt5 -->
 <div class="cf pv5 tc" style="min-height: 100vh;">
   <header class="primary-back tc lh-title mr-auto ml-auto w-90 w-80-ns w-80-m w-90-l">
-    <h2 class="f1 f2-ns f1-m f-headline-l fw7 mb0">We make Aid&nbsp;money<br>work as hard as it&nbsp;can</h2>
-    <h3 class="i georgia f3 tc fw1 mb5 w-100 w-100-ns w-80-m measure-l mr-auto ml-auto"><strong class="fw6">Clowns Without Borders</strong> understand how Play offers&nbsp;children &amp;&nbsp;young people the best protection from crisis situations.<br> Our range of tools are developed to support behaviour change,<br> &amp; deliver more effective Aid than our top 5 NGO Partners&nbsp;alone.</h3>
-    </header>
+    <!-- Early phase crisis psychosocial support for children -->
+    <h2 class="f1 f2-ns f1-m f-headline-l fw7 mb0">Laughter.<br>It's about the&nbsp;child</h2>
+    <h3 class="i georgia f3 tc fw1 mb5 w-100 w-100-ns w-80-m measure-l mr-auto ml-auto"><strong class="fw6">Clowns Without Borders</strong> is able to respond in the early&nbsp;phase of a crisis with psychosocial support for&nbsp;children,<br> such&nbsp;as our work in Ukraine.</h3>
+
+    <!-- <h2 class="f1 f2-ns f1-m f-headline-l fw7 mb0">We make Aid&nbsp;money<br>work as hard as it&nbsp;can</h2>
+    <h3 class="i georgia f3 tc fw1 mb5 w-100 w-100-ns w-80-m measure-l mr-auto ml-auto"><strong class="fw6">Clowns Without Borders</strong> understand how Play offers&nbsp;children &amp;&nbsp;young people the best protection from crisis situations.<br> Our range of tools are developed to support behaviour change,<br> &amp; deliver more effective Aid than our top 5 NGO Partners&nbsp;alone.</h3> -->
+  </header>
     <a href="#funding-partner" class="pointer link bg-transition br0 f6 f5-ns pv3 ba bw2 b--secondary-back ttu tracked-mega bg-primary-back hover-bg-accent w-third w-20-l b system tc white ph3 ph4-l">Become a Funding Partner</a>
 </div>
